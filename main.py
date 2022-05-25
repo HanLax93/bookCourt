e246c934c0b045c7251d61d52a8ae09406b5c1f2
han-lax <woodenhorsevirus@gmail.com>
2022-05-25 19:50:01 +0800

    update request time

--- main.py
+++ main.py
@@ -14,9 +14,9 @@ from processData import get_key
 def getParser():
     myParser = argparse.ArgumentParser(description='timeSetting')
     myParser.add_argument('--hour', type=int, default=12, help='setup hour part')
-    myParser.add_argument('--min', type=int, default=00, help='setup minute part')
-    myParser.add_argument('--sec', type=int, default=00, help='setup second part')
-    myParser.add_argument('--tms', type=int, default=700, help='setup microsecond part')
+    myParser.add_argument('--min', type=int, default=1, help='setup minute part')
+    myParser.add_argument('--sec', type=int, default=0, help='setup second part')
+    myParser.add_argument('--tms', type=int, default=300, help='setup microsecond part')
     a = myParser.parse_args()
     t_h = a.hour
     t_m = a.min
